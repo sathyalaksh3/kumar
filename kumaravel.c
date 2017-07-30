fb2482e9ea1700010d05d2cdff39b7e6432379b3
sathyalaksh3 <30349849+sathyalaksh3@users.noreply.github.com>
2017-07-30 11:58:09 +0530

    Create kumaravel.c

---
+++ kumaravel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+printf("vowels");
+else
+printf("consonants");
+getch();
+}
